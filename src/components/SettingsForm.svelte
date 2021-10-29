75a29485b51bf21d1c0049cc1101d161f32d86a1
Rex Walters <653208+wrex@users.noreply.github.com>
2021-10-29 16:53:46 -0700

    form includes all settings

--- src/components/SettingsForm.svelte
+++ src/components/SettingsForm.svelte
@@ -5,7 +5,58 @@
   <label for="retrieveDays">
     Number of days to retrieve reviews: 
   </label>
-  <input type="text" id="retrieveDays">
+  <input type="text" id="retrieveDays" />
+
+  <div>
+    <p>Display reviews or sessions per day?</p>
+    <label for="sessionsPerDay">
+      Sessions/day
+    </label>
+    <input type="radio" name="reviewSessions" id="sessionsPerDay" value="sessions"/>
+
+    <label for="reviewsPerDay">
+      Reviews/day
+    </label>
+    <input type="radio" name="reviewSessions" id="reviewsPerDay" value="reviews" />
+  </div>
+
+  <div>
+    <p>Theming colors</p>
+    <label for="bgColor">
+      Background
+    </label>
+    <input type="color" name="bgColor" id="bgColor" value="bgColor"/>
+
+    <label for="fillColor">
+      Fill
+    </label>
+    <input type="color" name="fillColor" id="fillColor" value="fillColor"/>
+
+    <label for="warnColor">
+      Warning
+    </label>
+    <input type="color" name="warnColor" id="warnColor" value="warnColor"/>
+
+    <label for="alertColor">
+      Alert
+    </label>
+    <input type="color" name="alertColor" id="alertColor" value="alertColor"/>
+  </div>
+
+  <label for="reviewsPer">Desired reviews per day or session</label>
+  <input type="number" name="reviewsPer" id="reviewsPer">
+
+  <label for="apprenticeItems">Desired number of apprentice items</label>
+  <input type="number" name="apprenticeItems" id="apprenticeItems">
+
+  <label for="acceptableMisses">Acceptable percentage of misses</label>
+  <input type="number" name="acceptableMisses" id="acceptableMisses">
+
+  <label for="newKanjiWeight">Weighting factor for new kanji</label>
+  <input type="number" name="newKanjiWeight" id="newKanjiWeight">
+
+  <label for="excessMissWeight">Weighting factor for excess misses</label>
+  <input type="number" name="excessMissWeight" id="excessMissWeight">
 
   <button>Save</button>
 </form>
--- src/components/SettingsForm.test.js
+++ src/components/SettingsForm.test.js
@@ -12,15 +12,58 @@ describe("Settings Form", () => {
       const input = screen.getByLabelText(/api key/i);
       expect(input).toBeInTheDocument();
     });
+    it("has a button to save settings", () => {
+      render(SettingsForm);
+      const button = screen.getByRole("button");
+      expect(button.textContent).toBe("Save");
+    });
     it("renders an input for the numnber of days to retrieve", () => {
       render(SettingsForm);
       const input = screen.getByLabelText(/number of days/i);
       expect(input).toBeInTheDocument();
     });
-    it("has a button to save settings", () => {
+    it("renders radio buttons for reviews/day or sessions/day", () => {
       render(SettingsForm);
-      const button = screen.getByRole("button");
-      expect(button.textContent).toBe("Save");
+      const option1 = screen.getByLabelText(/reviews\/day/i);
+      const option2 = screen.getByLabelText(/sessions\/day/i);
+      expect(option1).toBeInTheDocument();
+      expect(option2).toBeInTheDocument();
+    });
+    it("includes inputs for color theming", () => {
+      render(SettingsForm);
+      const [bgColor] = screen.getAllByLabelText(/background/i);
+      const [fillColor] = screen.getAllByLabelText(/fill/i);
+      const [warnColor] = screen.getAllByLabelText(/warning/i);
+      const [alertColor] = screen.getAllByLabelText(/alert/i);
+      expect(bgColor).toBeInTheDocument();
+      expect(fillColor).toBeInTheDocument();
+      expect(warnColor).toBeInTheDocument();
+      expect(alertColor).toBeInTheDocument();
+    });
+    it("includes an input for desired number of reviews", () => {
+      render(SettingsForm);
+      const input = screen.getByLabelText(/desired reviews per/i);
+      expect(input).toBeInTheDocument();
+    });
+    it("includes desired number of apprentice items", () => {
+      render(SettingsForm);
+      const input = screen.getByLabelText(/number of apprentice/i);
+      expect(input).toBeInTheDocument();
+    });
+    it("includes an input for percentage of acceptable misses", () => {
+      render(SettingsForm);
+      const input = screen.getByLabelText(/acceptable percentage of misses/i);
+      expect(input).toBeInTheDocument();
+    });
+    it("includes an input for new kanji weighting", () => {
+      render(SettingsForm);
+      const input = screen.getByLabelText(/new kanji/i);
+      expect(input).toBeInTheDocument();
+    });
+    it("includes an input for new extra misses", () => {
+      render(SettingsForm);
+      const input = screen.getByLabelText(/excess misses/i);
+      expect(input).toBeInTheDocument();
     });
   });
 });
