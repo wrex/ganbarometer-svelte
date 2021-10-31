cb29513472030344688103f3db60d61c9b7b3047
Rex Walters <653208+wrex@users.noreply.github.com>
2021-10-30 18:46:47 -0700

    fieldsets for form

--- src/components/SettingsForm.svelte
+++ src/components/SettingsForm.svelte
@@ -21,87 +21,108 @@
 </script>
 
 <form on:submit={handleSubmit} aria-label="Settings Form">
-  <label for="apiKey">API Key: </label>
-  <input 
+  <fieldset>
+    <legend>General</legend>
+    <label class="form-label" for="apiKey">API Key: </label>
+    <input 
+    class="form-control"
     type="text" 
     id="apiKey" 
     on:change={handleChange}
     on:blur={handleChange}
     bind:value={$form.apiKey}
-  />
-  {#if $errors.apiKey}
+    />
+    {#if $errors.apiKey}
     <small>Invalid token!</small>
-  {/if}
-
-  <label for="retrieveDays">
-    Number of days to retrieve reviews: 
-  </label>
-  <input type="text" id="retrieveDays" />
-
-  <div>
-    <p>Display reviews or sessions per day?</p>
-    <label for="sessionsPerDay">
-      Sessions/day
+    {/if}
+    
+    <label for="retrieveDays">
+      Number of days to retrieve reviews: 
     </label>
-    <input type="radio" name="reviewSessions" id="sessionsPerDay" value="sessions"/>
-
-    <label for="reviewsPerDay">
-      Reviews/day
-    </label>
-    <input type="radio" name="reviewSessions" id="reviewsPerDay" value="reviews" />
-  </div>
-
-  <div>
-    <p>Theming colors</p>
+    <input type="number" id="retrieveDays" />
+    
     <label for="bgColor">
       Background
     </label>
-    <input type="color" name="bgColor" id="bgColor" value="bgColor"/>
+    <input type="color" name="bgColor" id="bgColor" value="#ff0000" />
 
     <label for="fillColor">
       Fill
     </label>
-    <input type="color" name="fillColor" id="fillColor" value="fillColor"/>
+    <input type="color" name="fillColor" id="fillColor" value="red"/>
 
     <label for="warnColor">
       Warning
     </label>
-    <input type="color" name="warnColor" id="warnColor" value="warnColor"/>
+    <input type="color" name="warnColor" id="warnColor" value="red"/>
 
     <label for="alertColor">
       Alert
     </label>
-    <input type="color" name="alertColor" id="alertColor" value="alertColor"/>
-  </div>
+    <input type="color" name="alertColor" id="alertColor" value="red"/>
+  </fieldset>
 
-  <label for="reviewsPer">Desired reviews per day or session</label>
-  <input type="number" name="reviewsPer" id="reviewsPer">
+  <fieldset>
+    <legend>Pace Gauge</legend>
+    <div >
+      <label for="sessionsPerDay">
+        Sessions/day
+      </label>
+      <input type="radio" name="reviewSessions" id="sessionsPerDay" value="sessions"/>
+      <label for="reviewsPerDay">
+        Reviews/day
+      </label>
+      <input type="radio" name="reviewSessions" id="reviewsPerDay" value="reviews" />
+    </div>
 
-  <label for="apprenticeItems">Desired number of apprentice items</label>
-  <input type="number" name="apprenticeItems" id="apprenticeItems">
 
-  <label for="acceptableMisses">Acceptable percentage of misses</label>
-  <input type="number" name="acceptableMisses" id="acceptableMisses">
+    <label for="reviewsPer">Desired reviews per day or session</label>
+    <input type="number" name="reviewsPer" id="reviewsPer">
+  </fieldset>
 
-  <label for="newKanjiWeight">Weighting factor for new kanji</label>
-  <input type="number" name="newKanjiWeight" id="newKanjiWeight">
 
-  <label for="excessMissWeight">Weighting factor for excess misses</label>
-  <input type="number" name="excessMissWeight" id="excessMissWeight">
+  <fieldset>
+    <legend>Difficulty Gauge</legend>
+    <label for="apprenticeItems">Desired number of apprentice items</label>
+    <input type="number" name="apprenticeItems" id="apprenticeItems">
+
+    <label for="acceptableMisses">Acceptable percentage of misses</label>
+    <input type="number" name="acceptableMisses" id="acceptableMisses">
+
+    <label for="newKanjiWeight">Weighting factor for new kanji</label>
+    <input type="number" name="newKanjiWeight" id="newKanjiWeight">
+
+    <label for="excessMissWeight">Weighting factor for excess misses</label>
+    <input type="number" name="excessMissWeight" id="excessMissWeight">
+  </fieldset>
+
+  <fieldset>
+    <legend>Interval Chart</legend>
+    <label for="barStarts">Starting seconds for each bar</label>
+    <input type="string" name="barStarts" id="barStarts">
+    
+    <label for="barLabels">Labels for each bar</label>
+    <input type="string" name="barLabels" id="barLabels">
+  </fieldset>
+  
 
   <button type="submit">Save</button>
 </form>
 
 <style>
   form {
-    box-sizing: border-box;
-    width: 800px;
+    display: flex;
+    flex-direction: column;
   }
-  label {
-    display: inline;
+  /* label {
+    text-align: right;
+  } */
+  button {
+    padding: 0 1em;
   }
-  input[type="text"] {
-    display: block;
-    width: 400px;
+
+  input[type="color"] {
+    display: inline-block;
+    width: 20px;
   }
 </style>
