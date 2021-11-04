0f2d76a8237fb377a3810732c0135d247761d13c
Rex Walters <653208+wrex@users.noreply.github.com>
2021-11-04 12:16:54 -0700

    add Settings interface, load from localStorage

--- src/store/stores.ts
+++ src/store/stores.ts
@@ -1,11 +1,47 @@
 import { writable } from "svelte/store";
+import type { string } from "yup";
 
-const storedSettings = JSON.parse(
-  window.localStorage.getItem("gbSettings")
-) || { apiKey: "" };
+interface Settings {
+  apiKey: string;
+  retrieveDays: string;
+  bgColor: string;
+  fillColor: string;
+  warnColor: string;
+  alertColor: string;
+  reviewsPer: string;
+  apprenticeItems: string;
+  acceptableMisses: string;
+  newKanjiWeight: string;
+  excessMissWeight: string;
+  reviewSessions: string;
+}
+
+const storedSettings: Settings =
+  JSON.parse(window.localStorage.getItem("gbSettings")) || {};
 
 export const settings = writable({
   apiKey: storedSettings.apiKey ? storedSettings.apiKey : "",
+  retrieveDays: storedSettings.retrieveDays ? storedSettings.retrieveDays : "",
+  bgColor: storedSettings.bgColor ? storedSettings.bgColor : "",
+  fillColor: storedSettings.fillColor ? storedSettings.fillColor : "",
+  warnColor: storedSettings.warnColor ? storedSettings.warnColor : "",
+  alertColor: storedSettings.alertColor ? storedSettings.alertColor : "",
+  reviewsPer: storedSettings.reviewsPer ? storedSettings.reviewsPer : "",
+  apprenticeItems: storedSettings.apprenticeItems
+    ? storedSettings.apprenticeItems
+    : "",
+  acceptableMisses: storedSettings.acceptableMisses
+    ? storedSettings.acceptableMisses
+    : "",
+  newKanjiWeight: storedSettings.newKanjiWeight
+    ? storedSettings.newKanjiWeight
+    : "",
+  excessMissWeight: storedSettings.excessMissWeight
+    ? storedSettings.excessMissWeight
+    : "",
+  reviewSessions: storedSettings.reviewSessions
+    ? storedSettings.reviewSessions
+    : "",
 });
 
 settings.subscribe((value) => {
