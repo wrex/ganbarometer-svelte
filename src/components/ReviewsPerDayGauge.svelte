c128d54b1b56c3aa165c6dbf95c54b9c495ae20d
Rex Walters <653208+wrex@users.noreply.github.com>
2021-11-04 11:31:52 -0700

    style h1, p in ReviewsPerDayGauge

--- src/components/ReviewsPerDayGauge.svelte
+++ src/components/ReviewsPerDayGauge.svelte
@@ -7,3 +7,19 @@
   <Gauge value={0.65} label="127" />
   <p>Reviews/day footer</p>
 </div>
+
+<style>
+  h1 {
+    font-size: 1.25rem;
+    font-weight: normal;
+    margin: 0 0 5px;
+    text-align: center;
+    display: inline-block;
+    color: var(--text-color, #004033);
+  }
+
+  p {
+    color: var(--text-color, #004033);
+  }
+
+</style>
