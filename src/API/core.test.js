e66626cd47625ef63890057bc4da88c86a35a7aa
Rex Walters <653208+wrex@users.noreply.github.com>
2021-11-07 18:30:28 -0800

    remove "core" API (break into review/subject/etc)

--- src/API/core.test.js
+++
@@ -1,3 +0,0 @@
-it("does nothing", () => {
-  expect(1 + 1).toBe(2);
-});
--- src/API/core.ts
+++
@@ -1,7 +0,0 @@
-export const validApiKey = (key: string): boolean => {
-  return (
-    key.match(
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
-    ) !== null
-  );
-};
