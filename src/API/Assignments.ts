a7a01fd8e2f567c38c2592f75171121a873b283a
Rex Walters <653208+wrex@users.noreply.github.com>
2022-01-08 10:04:55 -0800

    comments re: expected daily reviews calculation

--- src/API/Assignments.ts
+++ src/API/Assignments.ts
@@ -24,22 +24,37 @@ export const getSrsCounts = async () => {
   const newKanji = newItems.filter((s) => s.object === "kanji");
   const newVocabulary = newItems.filter((s) => s.object === "vocabulary");
 
-  //This list means that for each SRS level this is how many days it takes until the item comes back
-  //For example: only 1/30 of master items are expected to be reviewed in any given day
-  //Notable figures
-  //Lessons: 0 days (they are not accounted for)
-  //Apprentice 1: 0.5 days (they come back as Apprentice 2 and will count as two reviews)
-  //Burned: 0 days
-  const srs_intervals = [0, 0.5, 1, 1, 2, 7, 14, 30, 120, 0];
-
   const allStages = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const reviewingStages = [1, 2, 3, 4, 5, 6, 7, 8];
 
   let counts: number[] = [];
   allStages.forEach((stage) => {
     counts[stage] = bySRS[stage]?.length ?? 0;
   });
 
+  //This list means that for each SRS level this is how many days it takes until
+  //the item becomes available.
+  //
+  //For example: only 1/30 of master items are expected to be reviewed in any
+  //given day
+  //
+  //Lessons & burns: 0 days (they are not accounted for)
+  //Apprentice 1: 0.5 days (they come back as Apprentice 2 and will count as two
+  //reviews)
+  //
+  // Rex's questions:
+  //   1. Shouldn't appr1 be 0.167 and appr2 0.333?
+  //
+  //      No. After you complete a lesson, the item becomes A1 and will be
+  //      available in 4 hours. Once you review the A1 item it comes back after
+  //      8 hours, not 4. Then when you review it as A2 it comes back after 24
+  //      hours. So even if you answer 100% correctly, you can only see an item
+  //      twice in one day.
+  //
+  const srs_intervals = [0, 0.5, 1, 1, 2, 7, 14, 30, 120, 0];
+
+  const reviewingStages = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  // First determine
   const expectedCount = reviewingStages
     .map((stage) => counts[stage] / srs_intervals[stage])
     .reduce((acc, count) => (acc += count));
